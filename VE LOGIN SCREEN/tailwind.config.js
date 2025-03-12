9ae930ec955ad7eac0280a6ebccfffd77e891164
mehdi-raza99 <mehdics100@gmail.com>
2025-03-12 07:30:02 +0500

    Login Screen Completed

---
+++ VE LOGIN SCREEN/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./**/*.html", // Add paths to all HTML files or other file types where Tailwind classes are used
+      // If you're using JSX, you might also include:
+      // "./**/*.jsx", 
+      // "./**/*.js", 
+      // etc.
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
